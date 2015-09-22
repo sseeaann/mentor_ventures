c61efd2c514e70b17d817903f9b08560b53fd2a5
Sean <seanhaines13@gmail.com>
2015-09-22 16:52:08 -0700

    shooting stars

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,85 @@
 $(function() {
-  $(window).stellar();
+
+  var clearTimout;
+
+  $(window).resize(function(){
+    if($(window).width() <= 1009){
+      $('.sunset').attr({
+        'data-stellar-background-ratio':'0.85',
+        'data-stellar-vertical-offset':'0'
+      });
+      $('.backTrees').attr({
+        'data-stellar-background-ratio':'1.10',
+        'data-stellar-vertical-offset':'-250'
+      });
+      $('.midTrees').attr({
+        'data-stellar-background-ratio':'1.15',
+        'data-stellar-vertical-offset':'-350'
+      });
+      $('.frontTrees').attr({
+        'data-stellar-background-ratio':'1.35',
+        'data-stellar-vertical-offset':'-900'
+      });
+    }
+    if($(window).width() > 1009){
+      $('.sunset').attr({
+        'data-stellar-background-ratio':'0.85',
+        'data-stellar-vertical-offset':'0'
+      });
+      $('.midTrees').attr({
+        'data-stellar-background-ratio':'1.20',
+        'data-stellar-vertical-offset':'-150'
+      });
+      $('.frontTrees').attr({
+        'data-stellar-background-ratio':'1.35',
+        'data-stellar-vertical-offset':'-600'
+      });
+    }
+    if(this.resizeTO) clearTimout(this.resizeTO);
+      this.resizeTO = setTimeout(function(){
+        $(this).trigger('reloadPage');
+      }, 500);
+  });
+
+  $(window).bind('reloadPage', function(){
+    location.reload();
+  });
+
+
+  if($(window).width() <= 1009){
+    $('.sunset').attr({
+      'data-stellar-background-ratio':'0.85',
+      'data-stellar-vertical-offset':'0'
+    });
+    $('.backTrees').attr({
+      'data-stellar-background-ratio':'1.10',
+      'data-stellar-vertical-offset':'-250'
+    });
+    $('.midTrees').attr({
+      'data-stellar-background-ratio':'1.15',
+      'data-stellar-vertical-offset':'-350'
+    });
+    $('.frontTrees').attr({
+      'data-stellar-background-ratio':'1.35',
+      'data-stellar-vertical-offset':'-900'
+    });
+    $.stellar();
+  }
+  if($(window).width() > 1009){
+    $('.sunset').attr({
+      'data-stellar-background-ratio':'0.85',
+      'data-stellar-vertical-offset':'0'
+    });
+    $('.midTrees').attr({
+      'data-stellar-background-ratio':'1.20',
+      'data-stellar-vertical-offset':'-150'
+    });
+    $('.frontTrees').attr({
+      'data-stellar-background-ratio':'1.35',
+      'data-stellar-vertical-offset':'-600'
+    });
+    $.stellar();
+  }
 
   var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
@@ -14,7 +94,7 @@ $(function() {
 
        setTimeout(function(){
          $('.makeout-point .animated').eq(i).addClass('treeShake');
-       }, (1200 * (Math.exp(i * 0.1))) - 1200);
+       }, (700 * (Math.exp(i * 0.1))) - 700);
 
      });
 
